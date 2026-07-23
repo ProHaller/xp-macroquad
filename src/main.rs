1b132f3320faf76788267da08817136979679d60
Roland Haller <haller.roland@gmail.com>
2026-07-23 17:17:43 +0200

    feat: square spawning

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,77 @@
 use macroquad::prelude::*;
 
+const MOVEMENT_SPEED: f32 = 800.0;
+
+struct Shape {
+    size: f32,
+    speed: f32,
+    x: f32,
+    y: f32,
+}
+
 #[macroquad::main("xp-macroquad")]
 async fn main() {
+    rand::srand(miniquad::date::now() as u64);
+    let mut squares = vec![];
+    let mut circle = Shape {
+        size: 32.0,
+        speed: MOVEMENT_SPEED,
+        x: screen_width() / 2.0,
+        y: screen_height() / 2.0,
+    };
+
     loop {
         clear_background(DARKPURPLE);
+
+        let delta_time = get_frame_time();
+        if is_key_down(KeyCode::Right) {
+            circle.x += MOVEMENT_SPEED * delta_time;
+        }
+        if is_key_down(KeyCode::Left) {
+            circle.x -= MOVEMENT_SPEED * delta_time;
+        }
+        if is_key_down(KeyCode::Down) {
+            circle.y += MOVEMENT_SPEED * delta_time;
+        }
+        if is_key_down(KeyCode::Up) {
+            circle.y -= MOVEMENT_SPEED * delta_time;
+        }
+
+        // Clamp X and Y to be within the screen
+        circle.x = clamp(circle.x, 0.0, screen_width());
+        circle.y = clamp(circle.y, 0.0, screen_height());
+
+        // Generate a new square
+        if rand::gen_range(0, 99) >= 95 {
+            let size = rand::gen_range(16.0, 64.0);
+            squares.push(Shape {
+                size,
+                speed: rand::gen_range(50.0, 150.0),
+                x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
+                y: -size,
+            });
+        }
+
+        // Move squares
+        for square in &mut squares {
+            square.y += square.speed * delta_time;
+        }
+
+        // Remove squares below bottom of screen
+        squares.retain(|square| square.y < screen_height() + square.size);
+
+        // Draw everything
+        draw_circle(circle.x, circle.y, circle.size / 2.0, YELLOW);
+        for square in &squares {
+            draw_rectangle(
+                square.x - square.size / 2.0,
+                square.y - square.size / 2.0,
+                square.size,
+                square.size,
+                GREEN,
+            );
+        }
+
         next_frame().await
     }
 }
